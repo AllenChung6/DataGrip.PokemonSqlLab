4e6ede09e9a8f0cccfb7ca1d3514d755d4388ae6
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-24 08:16:09 -0400

    Adjusted Part 4 answer. Finished lab.

--- answers/Part 4 Answers.sql
+++ answers/Part 4 Answers.sql
@@ -5,22 +5,22 @@
 #Sort the data by finding out which trainer has the strongest pokemon so that this will act as a ranking of strongest to weakest trainer.
 #You may interpret strongest in whatever way you want, but you will have to explain your decision.
 __________________________________________________________________________________________________________________________________________
-SELECT p.name, p.primary_type, p.secondary_type, t.trainername, SUM(pt.pokelevel)
+SELECT p.name, p.primary_type, p.secondary_type, t.trainername, AVG(pt.pokelevel)
 FROM pokemons p
 JOIN trainers t ON t.trainerID = p.ID
 JOIN pokemon_trainer pt ON t.trainerID = pt.trainerID
 GROUP BY p.name, p.primary_type, p.secondary_type, t.trainername, pt.pokelevel
-ORDER BY SUM(pt.pokelevel) DESC;
+ORDER BY AVG(pt.pokelevel) DESC;
 ------------------------------------------------------------------------------------------------------------------------------------------
-#Cooltrainer Vito is the strongest because I totaled the levels of all the pokemon each trainer has and Vito came out on top
-with a whopping 168 total pokemon level.
+# A-list Actor Alonso is the strongest Trainer because I averaged the levels of all the pokemon each trainer has and A-list Actor Alonso came out on top
+with an average whopping pokemon level score of 60.
 
 
 
 
 
-#Query to search Vitos pokemon
+#Query to search A-list Actor Alonso's' pokemon
 SELECT p.name, pt.pokelevel FROM pokemons p
 JOIN trainers t ON t.trainerid = p.id
 JOIN pokemon_trainer pt ON pt.trainerID = t.trainerid
-WHERE t.trainername = 'Cooltrainer♂ Vito';
+WHERE t.trainername = 'A-list Actor Alonso';
